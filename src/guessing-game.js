ebbcdc486bc7233b4af826616abac57d50d41d24
Evgenezak <zakharov.evgene@gmail.com>
2022-08-16 15:05:44 +0300

    feat: add fun

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min + 1;
+      this.max = max - 1;
     }
 
     guess() {
-
+      this.number = Math.ceil((this.min + this.max) / 2)
+      return this.number
     }
 
     lower() {
-
+      this.max = this.number - 1
     }
 
     greater() {
-
+      this.min = this.number + 1
     }
 }
 
